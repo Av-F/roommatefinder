dbe915bd51f5ba53e906c1c9fad86d6519dda8a1
Avi <felz527@gmail.com>
2026-03-30 10:57:26 -0400

    Fixed user

--- src/main/java/com/mates/roommatefinder/model/User.java
+++ src/main/java/com/mates/roommatefinder/model/User.java
@@ -15,13 +15,12 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
